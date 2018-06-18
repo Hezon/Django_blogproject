e6a2fa928b1316f5ebebe9a22046b7b38bf473a2
Hezon <jj72835@qq.com>
2018-06-18 16:48:02 +0800

    分类与归档

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,20 @@
 import markdown
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Category
 
 def index(request):
     post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time') # Python 中类实例调用属性的方法通常是 created_time.year，但是由于这里作为函数的参数列表，所以 Django 要求我们把点替换成了两个下划线，即 created_time__year。
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
     return render(request, 'blog/index.html', context={'post_list': post_list})
 
 def detail(request, pk):
